f0f83f01316d5580fa665742c36d76ad7eb1f9ff
Jba0901 <jassimpropvp@gmail.com>
2026-06-06 19:28:15 +0300

    Default site language to Arabic

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" dir="ltr">
+    <html lang="ar" dir="rtl">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
